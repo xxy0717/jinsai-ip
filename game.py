b021cdb078d30c0e2c3b51922e6872299b5bb123
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-15 13:11:29 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,28 +1,34 @@
-import streamlit as st
-import pymongo
+import os
 
-# 连接MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["ip_db"]
-collection = db["ip_addresses"]
+# 定义存储文件的路径
+file_path = 'ip_addresses.txt'
 
-# 页面标题和文本框
-st.title("IP地址审核")
-ip_address_a = st.text_input("请输入IP地址 A：")
-ip_address_b = st.text_input("请输入IP地址 B：")
+# 判断文件是否存在，如果不存在则创建
+if not os.path.exists(file_path):
+    with open(file_path, 'w') as f:
+        f.write('')
 
-# 提交按钮
-if st.button("提交"):
-    # 查询是否存在相同的记录
-    existing_record = collection.find_one({"ip_address_a": ip_address_a})
-    if existing_record:
-        # 如果存在相同记录，检查IP地址B是否相同
-        if existing_record["ip_address_b"] == ip_address_b:
-            st.warning("已有相同记录，可以通过")
-        else:
-            st.warning("已有不同的用户使用该IP，请驳回！")
-    else:
-        # 如果不存在相同记录，则插入新记录
-        new_record = {"ip_address_a": ip_address_a, "ip_address_b": ip_address_b}
-        collection.insert_one(new_record)
-        st.success("该IP为首次使用，请放心审核。")
+# 读取已经存在的IP地址信息
+with open(file_path, 'r') as f:
+    ip_addresses = f.read().splitlines()
+
+# 获取用户输入的IP地址信息
+ip_address_a = input('请输入IP地址a：')
+ip_address_b = input('请输入IP地址b：')
+
+# 判断IP地址a是否存在
+if ip_address_a in ip_addresses:
+    # 如果存在，则判断IP地址b是否和已存在的记录相同
+    with open(file_path, 'r') as f:
+        for line in f:
+            if line.startswith(ip_address_a):
+                if line.rstrip().split(',')[1] == ip_address_b:
+                    print('已有相同记录，可以通过')
+                else:
+                    print('已有不同的用户使用该IP，请驳回！')
+                break
+else:
+    # 如果不存在，则添加新的记录到文件中
+    with open(file_path, 'a') as f:
+        f.write(f'{ip_address_a},{ip_address_b}\n')
+    print('该IP为首次使用，请放心审核。')
