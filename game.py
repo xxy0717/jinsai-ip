2521ccff6d3b4045b6621e0877efd3e1ec7417e0
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-15 11:58:34 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,67 +1,28 @@
-import pygame
-import random
+import streamlit as st
+import pymongo
 
-# 初始化Pygame库
-pygame.init()
+# 连接MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["ip_db"]
+collection = db["ip_addresses"]
 
-# 定义屏幕大小
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 800
+# 页面标题和文本框
+st.title("IP地址审核")
+ip_address_a = st.text_input("请输入IP地址 A：")
+ip_address_b = st.text_input("请输入IP地址 B：")
 
-# 定义颜色
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# 定义方块大小
-BLOCK_SIZE = 20
-
-# 创建屏幕
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# 定义方块类
-class Block:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-    def move_down(self):
-        self.y += BLOCK_SIZE
-
-    def draw(self):
-        pygame.draw.rect(screen, self.color, [self.x, self.y, BLOCK_SIZE, BLOCK_SIZE])
-
-# 创建方块列表
-blocks = []
-for i in range(10):
-    blocks.append(Block(random.randint(0, SCREEN_WIDTH-BLOCK_SIZE), random.randint(0, SCREEN_HEIGHT-BLOCK_SIZE)))
-
-# 游戏循环
-running = True
-while running:
-    # 事件循环
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # 填充屏幕
-    screen.fill(BLACK)
-
-    # 绘制方块
-    for block in blocks:
-        block.move_down()
-        block.draw()
-
-        # 判断方块是否超出屏幕
-        if block.y > SCREEN_HEIGHT:
-            blocks.remove(block)
-            blocks.append(Block(random.randint(0, SCREEN_WIDTH-BLOCK_SIZE), random.randint(-SCREEN_HEIGHT, -BLOCK_SIZE)))
-
-    # 更新屏幕
-    pygame.display.flip()
-
-# 退出Pygame库
-pygame.quit()
+# 提交按钮
+if st.button("提交"):
+    # 查询是否存在相同的记录
+    existing_record = collection.find_one({"ip_address_a": ip_address_a})
+    if existing_record:
+        # 如果存在相同记录，检查IP地址B是否相同
+        if existing_record["ip_address_b"] == ip_address_b:
+            st.warning("已有相同记录，可以通过")
+        else:
+            st.warning("已有不同的用户使用该IP，请驳回！")
+    else:
+        # 如果不存在相同记录，则插入新记录
+        new_record = {"ip_address_a": ip_address_a, "ip_address_b": ip_address_b}
+        collection.insert_one(new_record)
+        st.success("该IP为首次使用，请放心审核。")
